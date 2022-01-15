31f02041651d240500ac084b1e1ee2b495e5703d
Thifany-Nicastro <thifanynicastro@hotmail.com>
2022-01-15 08:17:25 -0300

    Hooks e Truncate

--- test/specs/example.spec.ts
+++ test/specs/example.spec.ts
@@ -1,8 +1,17 @@
 import test from 'japa'
 import { request } from 'Test/utils'
 import { User } from 'App/Models'
+import Database from '@ioc:Adonis/Lucid/Database'
 
-test.group('Example', () => {
+test.group('Example', (group) => {
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
+  })
+
   test('ensure the login works', async (assert) => {
     await User.create({
       email: 'teste@teste.com',
@@ -11,7 +20,7 @@ test.group('Example', () => {
       name: 'teste'
     })
 
-    const { body, status } = await request
+    const { body } = await request
       .post('/auth')
       .send({
         email: 'teste@teste.com',
@@ -19,7 +28,23 @@ test.group('Example', () => {
       })
       .expect(200)
 
-    // assert.equal(status, 200)
+    assert.exists(body.token)
+  })
+
+  test('.............', async (assert) => {
+    await User.create({
+      email: 'teste@teste.com',
+      password: 'secret',
+      username: 'testeuser',
+      name: 'teste'
+    })
+
+    const { body, status } = await request.post('/auth').send({
+      email: 'teste@teste.com',
+      password: 'secret'
+    })
+
+    assert.equal(status, 200)
     assert.exists(body.token)
   })
 })

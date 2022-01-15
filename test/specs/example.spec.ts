0d8180e97a45021368f2ef2b43e3b9dd51e31175
Thifany-Nicastro <thifanynicastro@hotmail.com>
2022-01-15 08:10:45 -0300

    Interagindo com o DB

--- test/specs/example.spec.ts
+++ test/specs/example.spec.ts
@@ -1,11 +1,25 @@
 import test from 'japa'
 import { request } from 'Test/utils'
+import { User } from 'App/Models'
 
 test.group('Example', () => {
-  test('ensure the root route works', async (assert) => {
-    const { body } = await request.get('/')
+  test('ensure the login works', async (assert) => {
+    await User.create({
+      email: 'teste@teste.com',
+      password: 'secret',
+      username: 'testeuser',
+      name: 'teste'
+    })
 
-    assert.exists(body.hello)
-    assert.equal(body.hello, 'world')
+    const { body, status } = await request
+      .post('/auth')
+      .send({
+        email: 'teste@teste.com',
+        password: 'secret'
+      })
+      .expect(200)
+
+    // assert.equal(status, 200)
+    assert.exists(body.token)
   })
 })
